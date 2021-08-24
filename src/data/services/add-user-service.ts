9b8f83484403484434425db5f68ce51eb7516072
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-24 01:14:38 -0300

    feat: ensure AddUserService returns an UsernameInUseError if CheckRepo returns true

--- src/data/services/add-user-service.ts
+++ src/data/services/add-user-service.ts
@@ -1,11 +1,15 @@
 import { CheckUserByUsernameRepository } from '@/data/contracts/repos'
+import { UsernameInUseError } from '@/domain/entities/errors'
 import { AddUser } from '@/domain/use-cases'
 
 export class AddUserService implements AddUser {
-  constructor (private readonly checkUserByUsernameRepository: CheckUserByUsernameRepository) {}
+  constructor (private readonly checkUserByUsernameRepository: CheckUserByUsernameRepository) { }
 
   async add ({ username }: AddUser.Params): Promise<AddUser.Result> {
-    await this.checkUserByUsernameRepository.check({ username })
+    const exists = await this.checkUserByUsernameRepository.check({ username })
+    if (exists) {
+      return new UsernameInUseError()
+    }
     return new Error()
   }
 }
