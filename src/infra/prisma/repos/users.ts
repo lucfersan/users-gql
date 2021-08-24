0965a10ea1581867c30690ae96767292b03d09cf
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-24 04:57:52 -0300

    feat: ensure PrismaUsersRepository returns a basic user on success

--- src/infra/prisma/repos/users.ts
+++ src/infra/prisma/repos/users.ts
@@ -1,9 +1,18 @@
-import { CheckUserByUsernameRepository } from '@/data/contracts/repos'
+import { AddUserRepository, CheckUserByUsernameRepository } from '@/data/contracts/repos'
 import prisma from '@/infra/prisma/client'
 
-export class PrismaUsersRepository implements CheckUserByUsernameRepository {
+export class PrismaUsersRepository implements CheckUserByUsernameRepository, AddUserRepository {
   async check ({ username }: CheckUserByUsernameRepository.Params): Promise<CheckUserByUsernameRepository.Result> {
     const user = await prisma.user.findUnique({ where: { username } })
     return !!user
+  }
+
+  async add (params: AddUserRepository.Params): Promise<AddUserRepository.Result> {
+    const { id, firstName, username } = await prisma.user.create({ data: params })
+    return {
+      id,
+      firstName,
+      username
+    }
   }
 }
