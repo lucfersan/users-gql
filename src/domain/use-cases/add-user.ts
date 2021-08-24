3f1b61eeb75c41388722e77d6b4854302f9f850f
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-24 00:21:37 -0300

    feat: create AddUser use case

---
+++ src/domain/use-cases/add-user.ts
@@ -0,0 +1,19 @@
+export interface AddUser {
+  add: (params: AddUser.Params) => Promise<AddUser.Result>
+}
+
+export namespace AddUser {
+  export type Params = {
+    firstName: string
+    lastName: string
+    username: string
+    age: number
+    password: string
+  }
+
+  export type Result = {
+    id: string
+    username: string
+    firstName: string
+  }
+}
