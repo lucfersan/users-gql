fd77457361888801de2c59ba4b280190772890d1
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-25 01:00:22 -0300

    feat: ensure BcryptAdapter returns a digest on success

--- src/infra/crypto/bcrypt-adapter.ts
+++ src/infra/crypto/bcrypt-adapter.ts
@@ -6,7 +6,7 @@ export class BcryptAdapter implements Hasher {
   constructor (private readonly salt: number) {}
 
   async hash ({ plaintext }: Hasher.Params): Promise<Hasher.Result> {
-    await bcrypt.hash(plaintext, this.salt)
-    return ''
+    const digest = await bcrypt.hash(plaintext, this.salt)
+    return digest
   }
 }
