cb2c1c75b7fdd14c5eec0f3f6825f5168539cc6d
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-25 05:24:50 -0300

    test: ensure addUser mutation adds a user

---
+++ tests/main/graphql/users.test.ts
@@ -0,0 +1,53 @@
+import { ApolloServer, gql } from 'apollo-server-express'
+
+import prisma from '@/infra/prisma/client'
+import { mockAddUserParams } from '@/tests/domain/mocks/use-cases'
+import { makeApolloServer } from '@/tests/main/graphql/mocks'
+
+describe('Users GraphQL', () => {
+  let apolloServer: ApolloServer
+
+  beforeAll(async () => {
+    apolloServer = makeApolloServer()
+  })
+
+  afterAll(async () => {
+    await prisma.$disconnect()
+  })
+
+  describe('addUser Mutation', () => {
+    const addUserMutation = gql`
+      mutation (
+        $firstName: String!
+        $lastName: String!
+        $username: String!
+        $age: Int!
+        $password: String!
+      ) {
+        addUser(
+          firstName: $firstName
+          lastName: $lastName
+          username: $username
+          age: $age
+          password: $password
+        ) {
+          id
+          firstName
+          username
+        }
+      }
+    `
+
+    it('should add a user', async () => {
+      const params = mockAddUserParams()
+      const res = await apolloServer.executeOperation({
+        query: addUserMutation,
+        variables: params
+      })
+      expect(res.errors).toBeUndefined()
+      expect(res.data?.addUser.id).toBeDefined()
+      expect(res.data?.addUser.firstName).toBe(params.firstName)
+      expect(res.data?.addUser.username).toBe(params.username)
+    })
+  })
+})
