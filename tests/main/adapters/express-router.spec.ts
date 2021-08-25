42732e46eefea4bd3236573f11f2937efd479f8c
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-25 02:52:06 -0300

    test: ensure ExpressRouterAdapter responds with 500 and valid error

--- tests/main/adapters/express-router.spec.ts
+++ tests/main/adapters/express-router.spec.ts
@@ -90,4 +90,16 @@ describe('ExpressRouterAdapter', () => {
     expect(res.json).toHaveBeenCalledWith({ error: 'error_four_hundred' })
     expect(res.json).toHaveBeenCalledTimes(1)
   })
+
+  it('should respond with 500 and valid error', async () => {
+    controller.handle.mockResolvedValue({
+      statusCode: 500,
+      data: new Error('error_five_hundred')
+    })
+    await sut(req, res, next)
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ error: 'error_five_hundred' })
+    expect(res.json).toHaveBeenCalledTimes(1)
+  })
 })
