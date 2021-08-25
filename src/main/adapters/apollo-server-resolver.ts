52472d154ac9ddb70e0f9852ef4a2a7449bc26a3
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-25 05:02:19 -0300

    feat: add status code error on exceptions

--- src/main/adapters/apollo-server-resolver.ts
+++ src/main/adapters/apollo-server-resolver.ts
@@ -1,6 +1,14 @@
+import { ApolloError, UserInputError } from 'apollo-server-express'
+
 import { Controller } from '@/application/helpers'
 
 export const adaptApolloResolver = async (controller: Controller, args?: any): Promise<any> => {
-  const httpResponse = await controller.handle({ ...args })
-  return httpResponse.data
+  const { statusCode, data } = await controller.handle({ ...args })
+  switch (statusCode) {
+    case 200:
+    case 201:
+    case 204: return data
+    case 400: throw new UserInputError(data.message)
+    default: throw new ApolloError(data.message)
+  }
 }
--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -1,13 +1,34 @@
 import { ApolloServer } from 'apollo-server-express'
 import { Express } from 'express'
+import { GraphQLError } from 'graphql'
 
 import resolvers from '@/main/graphql/resolvers'
 import typeDefs from '@/main/graphql/type-defs'
 
+const checkError = (error: GraphQLError, errorName: string): boolean => {
+  return [error.name, error.originalError?.name].some(name => name === errorName)
+}
+
+const handleErrors = (response: any, errors: readonly GraphQLError[] | undefined): void => {
+  errors?.forEach(error => {
+    response.data = undefined
+    if (checkError(error, 'UserInputError')) {
+      response.http.status = 400
+    } else {
+      response.http.status = 500
+    }
+  })
+}
+
 export const setupApollo = async (app: Express): Promise<void> => {
   const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    plugins: [{
+      requestDidStart: async () => ({
+        willSendResponse: async ({ response, errors }) => handleErrors(response, errors)
+      })
+    }]
   })
   await server.start()
   server.applyMiddleware({ app })
