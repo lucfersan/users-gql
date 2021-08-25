f1b5413249fb4841dd3cffd27ad1181ea8b5cba0
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-25 02:45:05 -0300

    feat: apply adapter to express route

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,11 +1,8 @@
 import { Router } from 'express'
 
+import { adaptExpressRoute } from '@/main/adapters'
 import { makeAddUserController } from '@/main/factories/controllers'
 
 export default (router: Router): void => {
-  router.post('/users', async (req, res) => {
-    const { statusCode, data } = await makeAddUserController().handle({ ...req.body })
-    const json = statusCode >= 200 && statusCode <= 299 ? data : { error: data.message }
-    return res.status(statusCode).json(json)
-  })
+  router.post('/users', adaptExpressRoute(makeAddUserController()))
 }
