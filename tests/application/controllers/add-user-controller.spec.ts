f525ff34b8ec3727df198ed6aa2355bd5a327a5d
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-24 23:11:53 -0300

    refactor: make validateRequestFields a private function

--- tests/application/controllers/add-user-controller.spec.ts
+++ tests/application/controllers/add-user-controller.spec.ts
@@ -7,16 +7,24 @@ type HttpRequest = {
   age: number
   password: string
 }
+
 class AddUserController {
   async handle (httpRequest: HttpRequest): Promise<any> {
+    const error = this.validateRequestFields(httpRequest)
+    if (error) {
+      return {
+        statusCode: 400,
+        data: error
+      }
+    }
+  }
+
+  private validateRequestFields (httpRequest: HttpRequest): Error | undefined {
     const requiredFields = Object.entries(httpRequest)
     for (const field of requiredFields) {
       const [key, value] = field
       if (!value) {
-        return {
-          statusCode: 400,
-          data: new Error(`Required field: ${key}`)
-        }
+        return new Error(`Required field: ${key}`)
       }
     }
   }
