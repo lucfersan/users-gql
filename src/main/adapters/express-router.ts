6b2a98d808ce94853bfcf97b281f7f456bc9d3d4
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-25 02:31:20 -0300

    feat: ensure ExpressRouterAdapter calls handle with correct request

---
+++ src/main/adapters/express-router.ts
@@ -0,0 +1,11 @@
+import { RequestHandler } from 'express'
+
+import { Controller } from '@/application/helpers'
+
+type Adapter = (controller: Controller) => RequestHandler
+
+export const adaptExpressRoute: Adapter = controller => async (req, res) => {
+  const { statusCode, data } = await controller.handle({ ...req.body })
+  const json = [201].includes(statusCode) ? data : { error: data.message }
+  return res.status(statusCode).json(json)
+}
