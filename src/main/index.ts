f15846e35f64ba7e250017f36c9093a8151da8ae
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-25 01:19:47 -0300

    feat: setup express middlewares

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,4 @@
-import express from 'express'
-
-const app = express()
+import './config/module-alias'
+import { app } from '@/main/config/app'
 
 app.listen(3333, () => console.log('Server running at 3333 🔥'))
