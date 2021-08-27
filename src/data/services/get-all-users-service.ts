4ddbce6f3f3999002fb9f74e7fb7d76523fd8287
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-27 01:38:58 -0300

    feat: ensure GetAllUsersService returns a list of Users on success

--- src/data/services/get-all-users-service.ts
+++ src/data/services/get-all-users-service.ts
@@ -5,13 +5,7 @@ export class GetAllUsersService implements GetAllUsers {
   constructor (private readonly getAllUsersRepository: GetAllUsersRepository) {}
 
   async get (): Promise<GetAllUsers.Result> {
-    await this.getAllUsersRepository.getAll()
-    return [{
-      id: 'id',
-      firstName: 'firstName',
-      lastName: 'lastName',
-      username: 'username',
-      age: 100
-    }]
+    const users = this.getAllUsersRepository.getAll()
+    return users
   }
 }
