24aa80f8341aff1b2b3c744ca72b7f0824ecf733
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-27 02:24:50 -0300

    feat: ensure GetAllUsersController returns a serverError if GetAllUsers throws

--- src/application/controllers/get-all-users-controller.ts
+++ src/application/controllers/get-all-users-controller.ts
@@ -1,4 +1,4 @@
-import { HttpResponse, Controller } from '@/application/helpers'
+import { HttpResponse, Controller, serverError } from '@/application/helpers'
 import { GetAllUsers } from '@/domain/use-cases'
 
 type HttpRequest = {}
@@ -7,10 +7,14 @@ export class GetAllUsersController implements Controller {
   constructor (private readonly getAllUsers: GetAllUsers) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.getAllUsers.get()
-    return {
-      statusCode: 200,
-      data: {}
+    try {
+      await this.getAllUsers.get()
+      return {
+        statusCode: 200,
+        data: {}
+      }
+    } catch {
+      return serverError()
     }
   }
 }
