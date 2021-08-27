39d8aee698f8f6532837a94c9279084c6e17045d
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-27 02:26:21 -0300

    test: ensure GetAllUsersController returns a serverError if GetAllUsers throws

--- tests/application/controllers/get-all-users-controller.spec.ts
+++ tests/application/controllers/get-all-users-controller.spec.ts
@@ -1,4 +1,6 @@
 import { GetAllUsersController } from '@/application/controllers'
+import { serverError } from '@/application/helpers'
+import { throwError } from '@/tests/domain/mocks'
 import { GetAllUsersSpy } from '@/tests/domain/mocks/use-cases'
 
 type SutTypes = {
@@ -20,5 +22,12 @@ describe('GetAllUsersController', () => {
     const { sut, getAllUsersSpy } = makeSut()
     await sut.handle({})
     expect(getAllUsersSpy.callsCount).toBe(1)
+  })
+
+  it('should return a serverError if GetAllUsers throws', async () => {
+    const { sut, getAllUsersSpy } = makeSut()
+    jest.spyOn(getAllUsersSpy, 'get').mockImplementationOnce(throwError)
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(serverError())
   })
 })
