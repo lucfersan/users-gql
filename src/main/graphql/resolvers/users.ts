fa7b2924edbdaec8ddb3edcd277b31f90cb01ac1
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-27 03:36:14 -0300

    feat: ensure getAllUsers query returns all users

--- src/main/graphql/resolvers/users.ts
+++ src/main/graphql/resolvers/users.ts
@@ -1,8 +1,12 @@
 import { adaptApolloResolver } from '@/main/adapters'
-import { makeAddUserController } from '@/main/factories/controllers'
+import { makeAddUserController, makeGetAllUsersController } from '@/main/factories/controllers'
 
 export default {
   Mutation: {
     addUser: async (parent: any, args: any) => adaptApolloResolver(makeAddUserController(), args)
+  },
+
+  Query: {
+    getAllUsers: async () => adaptApolloResolver(makeGetAllUsersController())
   }
 }
--- src/main/graphql/type-defs/users.ts
+++ src/main/graphql/type-defs/users.ts
@@ -2,10 +2,22 @@ import { gql } from 'apollo-server-express'
 
 export default gql`
   extend type Mutation {
-    addUser (firstName: String!, lastName: String!, username: String!, age: Int!, password: String!): User!
+    addUser (firstName: String!, lastName: String!, username: String!, age: Int!, password: String!): BasicUser!
+  }
+
+  extend type Query {
+    getAllUsers: [User!]!
   }
 
   type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    username: String!
+    age: Int!
+  }
+
+  type BasicUser {
     id: ID!
     firstName: String!
     username: String!
