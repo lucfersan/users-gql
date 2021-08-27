ed492b49ad853bdbfd03a6f16e7b5a543ee8cdf7
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-27 02:01:15 -0300

    feat: ensure PrismaUsersRepository returns a list of users on success

--- src/infra/prisma/repos/users.ts
+++ src/infra/prisma/repos/users.ts
@@ -1,7 +1,7 @@
-import { AddUserRepository, CheckUserByUsernameRepository } from '@/data/contracts/repos'
+import { AddUserRepository, CheckUserByUsernameRepository, GetAllUsersRepository } from '@/data/contracts/repos'
 import prisma from '@/infra/prisma/client'
 
-export class PrismaUsersRepository implements CheckUserByUsernameRepository, AddUserRepository {
+export class PrismaUsersRepository implements CheckUserByUsernameRepository, AddUserRepository, GetAllUsersRepository {
   async check ({ username }: CheckUserByUsernameRepository.Params): Promise<CheckUserByUsernameRepository.Result> {
     const user = await prisma.user.findUnique({ where: { username } })
     return !!user
@@ -14,5 +14,18 @@ export class PrismaUsersRepository implements CheckUserByUsernameRepository, Add
       firstName,
       username
     }
+  }
+
+  async getAll (): Promise<GetAllUsersRepository.Result> {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        age: true,
+        username: true
+      }
+    })
+    return users
   }
 }
