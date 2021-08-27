106d7837b7215a76b6dcb24e2fc10f516785939c
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-26 21:12:53 -0300

    feat: add dotenv config before app listen

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,10 @@
 import './config/module-alias'
+import { config } from 'dotenv-flow'
+
 import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
+
+config({ silent: true })
 
 app.listen(env.port, () => {
   console.log(`Server started at port ${env.port} 🔥`)
