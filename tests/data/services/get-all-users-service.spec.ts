b5731bebca72997dc8733b9ed2d09dfc3bf2cc98
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-27 01:33:31 -0300

    test: ensure GetAllUsersService throws if GetAllUsersRepository throws

--- tests/data/services/get-all-users-service.spec.ts
+++ tests/data/services/get-all-users-service.spec.ts
@@ -1,5 +1,6 @@
 import { GetAllUsersService } from '@/data/services'
 import { GetAllUsersRepositorySpy } from '@/tests/data/mocks/contracts/repos'
+import { throwError } from '@/tests/domain/mocks'
 
 type SutTypes = {
   sut: GetAllUsersService
@@ -20,5 +21,12 @@ describe('GetAllUsersService', () => {
     const { sut, getAllUsersRepositorySpy } = makeSut()
     await sut.get()
     expect(getAllUsersRepositorySpy.callsCount).toBe(1)
+  })
+
+  it('should throw if GetAllUsersRepository throws', async () => {
+    const { sut, getAllUsersRepositorySpy } = makeSut()
+    jest.spyOn(getAllUsersRepositorySpy, 'getAll').mockImplementationOnce(throwError)
+    const promise = sut.get()
+    await expect(promise).rejects.toThrow()
   })
 })
