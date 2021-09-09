7398ac84d9f21027d70d10122e10d1759f4ced75
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-09-09 15:37:55 -0300

    feat: add AuthenticateUserController

---
+++ src/application/controllers/authenticate-user.ts
@@ -0,0 +1,38 @@
+import { RequiredFieldError } from '@/application/errors'
+import { badRequest, ok, HttpResponse, serverError, Controller } from '@/application/helpers'
+import { AuthenticationError } from '@/domain/entities/errors'
+import { AuthenticateUser } from '@/domain/use-cases'
+
+type HttpRequest = {
+  username: string
+  password: string
+}
+
+export class AuthenticateUserController implements Controller {
+  constructor (private readonly authenticateUser: AuthenticateUser) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const error = this.validateRequestFields(httpRequest)
+      if (error) {
+        return badRequest(error)
+      }
+      const result = await this.authenticateUser.auth(httpRequest)
+      if (result instanceof AuthenticationError) {
+        return badRequest(new AuthenticationError())
+      }
+      return ok(result)
+    } catch {
+      return serverError()
+    }
+  }
+
+  private validateRequestFields (request: any): Error | undefined {
+    const requiredFields = ['username', 'password']
+    for (const field of requiredFields) {
+      if (!request[field]) {
+        return new RequiredFieldError(field)
+      }
+    }
+  }
+}
