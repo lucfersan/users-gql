95836b7abe16a98cc71fec11526b27a141a45bbd
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-09-09 15:40:10 -0300

    test: add AuthenticateUserController

---
+++ tests/application/controllers/authenticate-user.spec.ts
@@ -0,0 +1,58 @@
+import { AuthenticateUserController } from '@/application/controllers'
+import { RequiredFieldError } from '@/application/errors'
+import { badRequest, ok, serverError } from '@/application/helpers'
+import { AuthenticationError } from '@/domain/entities/errors'
+import { throwError } from '@/tests/domain/mocks'
+import { AuthenticateUserSpy, mockAuthParams } from '@/tests/domain/mocks/use-cases'
+
+type SutTypes = {
+  sut: AuthenticateUserController
+  authenticateUserSpy: AuthenticateUserSpy
+}
+
+const makeSut = (): SutTypes => {
+  const authenticateUserSpy = new AuthenticateUserSpy()
+  const sut = new AuthenticateUserController(authenticateUserSpy)
+  return {
+    sut,
+    authenticateUserSpy
+  }
+}
+
+describe('AuthenticateUserController', () => {
+  it('should return a badRequest if one of the required fields is not provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({
+      ...mockAuthParams(),
+      username: ''
+    })
+    expect(httpResponse).toEqual(badRequest(new RequiredFieldError('username')))
+  })
+
+  it('should call AuthenticateUser with correct values', async () => {
+    const { sut, authenticateUserSpy } = makeSut()
+    const params = mockAuthParams()
+    await sut.handle(params)
+    expect(authenticateUserSpy.params).toEqual(params)
+  })
+
+  it('should return a serverError if AuthenticateUser throws', async () => {
+    const { sut, authenticateUserSpy } = makeSut()
+    jest.spyOn(authenticateUserSpy, 'auth').mockImplementationOnce(throwError)
+    const httpResponse = await sut.handle(mockAuthParams())
+    expect(httpResponse).toEqual(serverError())
+  })
+
+  it('should return a badRequest with AuthenticationError if the username already exists', async () => {
+    const { sut, authenticateUserSpy } = makeSut()
+    authenticateUserSpy.result = new AuthenticationError()
+    const httpResponse = await sut.handle(mockAuthParams())
+    expect(httpResponse).toEqual(badRequest(new AuthenticationError()))
+  })
+
+  it('should return ok on success', async () => {
+    const { sut, authenticateUserSpy } = makeSut()
+    const httpResponse = await sut.handle(mockAuthParams())
+    expect(httpResponse).toEqual(ok(authenticateUserSpy.result))
+  })
+})
--- tests/domain/mocks/use-cases/authenticate-user.ts
+++ tests/domain/mocks/use-cases/authenticate-user.ts
@@ -6,3 +6,18 @@ export const mockAuthParams = (): AuthenticateUser.Params => ({
   username: faker.internet.userName(),
   password: faker.internet.password()
 })
+
+export class AuthenticateUserSpy implements AuthenticateUser {
+  result: AuthenticateUser.Result = {
+    id: faker.datatype.uuid(),
+    firstName: faker.name.firstName(),
+    token: faker.datatype.uuid()
+  }
+
+  params: AuthenticateUser.Params
+
+  async auth (params: AuthenticateUser.Params): Promise<AuthenticateUser.Result> {
+    this.params = params
+    return this.result
+  }
+}
